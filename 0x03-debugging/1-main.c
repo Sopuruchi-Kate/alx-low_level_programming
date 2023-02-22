516ee216df6687db2411a74b80b329cb50d359b4
Sopuruchi-Kate <katedaniel775@gmail.com>
2023-02-22 08:48:46 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,16 +6,19 @@
  */
 
 int main(void)
-
 {
-	int i;
+int i;
 
-	printf("infinite loop incoming :(\n");
-	i = 0;
-	/*while (i < 10)*/
-	/*{*/
-	/*	putchar(i);*/
-	/*}*/
-	printf("infinite loop avoided! \\0/\n");
-	return (0);
+printf("infinite loop incoming :(\n");
+
+i = 0;
+/*
+ * while (i < 10)
+ * {
+ *	 putchar(i);
+ * }
+ */
+printf("infinite loop avoided! \\0/\n");
+
+return (0);
 }
