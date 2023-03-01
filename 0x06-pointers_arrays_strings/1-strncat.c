18bc7a022d9b20cc90b98ed32e2cbf5b7a1b24a7
Sopuruchi-Kate <katedaniel775@gmail.com>
2023-03-01 07:33:59 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@
  * using at most a number of bytes from src
  * @dest: the string to be appended
  * @src: the string to be appended to dest
- * @n: th number of bytes from src to be appended to dest
+ * @n: the number of bytes from src to be appended to dest
  * Return: a pointer to the resulting string dest
  */
 
@@ -17,6 +17,6 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[index++])
 		dest_len++;
 	for (index = 0; src[index] && index < n; index++)
-	       dest[dest_len++] = src[index];
+		dest[dest_len++] = src[index];
 	return (dest);
 }
