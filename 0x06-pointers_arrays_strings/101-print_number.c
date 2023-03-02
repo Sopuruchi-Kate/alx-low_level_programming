cc25aae9b63aad944980059f107b4f87e0f356c9
Sopuruchi-Kate <katedaniel775@gmail.com>
2023-03-02 14:39:02 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,14 +9,13 @@ void print_number(int n)
 {
 	unsigned int num = n;
 
-	if (n < o)
+	if (n < 0)
 	{
 		_putchar('-');
-		num - -num;
+		num = -num;
 	}
 
 	if ((num / 10) > 0)
 		print_number(num / 10);
-
 	_putchar((num % 10) + '0');
 }
